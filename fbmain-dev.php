24e372cc7d13ca9b86833dfc2f15383e8736323d
Rodrigo Ayala <azul.rejected@gmail.com>
2012-08-27 03:18:54 -0500

    Se agrego friends_events al scope de permisos para futura mineria de datos

--- fbmain-dev.php
+++ fbmain-dev.php
@@ -3,7 +3,7 @@
     $fbconfig['appid' ] = "490859974259565";
     $fbconfig['secret'] = "7a9fcfd7d757e1190477342b962207ce";
 	
-	$fbconfig['scope']  = 'email,user_about_me,friends_about_me,friends_birthday,friends_education_history,friends_interests,friends_likes,friends_subscriptions,read_friendlists,publish_stream,publish_actions';
+	$fbconfig['scope']  = 'email,user_about_me,friends_about_me,friends_birthday,friends_education_history,friends_events,friends_interests,friends_likes,friends_subscriptions,read_friendlists,publish_stream,publish_actions';
 
     $fbconfig['baseUrl']    =   "localhost/friendfinder/";//
     $fbconfig['appBaseUrl'] =   "http://apps.facebook.com/fb-firend-finder-dev/";
